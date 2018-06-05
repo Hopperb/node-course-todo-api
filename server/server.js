d1cc89eb830cc198033045510611eeca3236699b
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-05 01:53:01 -0400

    Add POST /todos route and refactored mongoose

--- server/server.js
+++ server/server.js
@@ -1,64 +1,26 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {mongoose} = require('./db/mongoose');
+const  {Todo} = require('./models/Todo');
+const {User} = require('./models/User');
 
-// const Todo = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true,
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
+const app = express();
 
-// const newTodo = new Todo({
-//     text: "Walk the dog",
-//     completed: true,
-//     completedAt: 1
-// });
+app.use(bodyParser.json());
 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-// const otherTodo = new Todo({
-//     text: "Take over the world",
-//     completed: false,
-//     completedAt: 2
-// })
-
-// otherTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-const User = mongoose.model('User', {
-    email: { 
-    type: String,
-    require: true,
-    trim: true,
-    minlength: 1
-    }
+app.post('/todos', (req, res)=> {
+    console.log(req.body);
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
 })
 
-const newUser = new User({
-    email: "brandon@mail.com"
+app.listen(3000, () => {
+    console.log('Started on port 3000')
 })
-
-newUser.save().then((doc) => {
-    console.log('Saved User', doc)
-}, (e) => {
-    console.log('Unable to save User')
-});
