3cf0c3be74fb097897e50a82531715d95b33d45e
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-15 03:15:30 -0400

    - setup start script and heroku port

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/Todo');
 
 const todos = [{
-  text: 'first text todo'
+    _id: new ObjectID(),
+    text: 'first test todo'
 }, {
+  _id: new ObjectID(),
   text: 'second test todo'
 }]
 
@@ -69,3 +72,15 @@ describe('GET /todos', () => {
      .end(done);
   })
 })
+
+describe('GET todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+});
